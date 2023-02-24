48cd83b8b64a7d4914db95e43495981e3f0e4ec1
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2023-02-24 20:25:24 +0300

    scss extends

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ghpages = require('gh-pages');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 ghpages.publish('dist', function (err) { });
 
@@ -46,6 +47,7 @@ module.exports = {
             template: path.resolve(__dirname, 'src', 'index.html'),
             filename: 'index.html',
         }),
+        new FaviconsWebpackPlugin('./src/images/Ellipse.ico'),
         new FileManagerPlugin({
             events: {
                 onEnd: {
